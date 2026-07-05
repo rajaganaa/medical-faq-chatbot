20db5c5685ca08a4cb58cf8c218be89b40283879
Rajaganapathy M <rajaganaa@gmail.com>
2026-07-05 12:05:11 +0530

    [agent] T02 (coder): task complete

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ChatbotPage from './pages/ChatbotPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chatbot" element={<ChatbotPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/ChatbotPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatbotPage = () => {
+  return (
+    <div>
+      <h1>Chatbot Page</h1>
+    </div>
+  );
+};
+
+export default ChatbotPage;
