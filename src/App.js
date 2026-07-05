76326cc327e7659bbeee929f907e643b4878c316
Rajaganapathy M <rajaganaa@gmail.com>
2026-07-05 12:06:20 +0530

    [agent] T03 (coder): task complete

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ChatbotPage from './pages/ChatbotPage';
+import Header from './Header';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/chatbot" element={<ChatbotPage />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      My App Header
+    </header>
+  );
+};
+
+export default Header;
