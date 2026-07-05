c875c7389cdcfd6a05da9abb5dc9dec15327fc1b
Rajaganapathy M <rajaganaa@gmail.com>
2026-07-05 07:03:15 +0530

    [agent] T02 (coder): task complete

--- database.py
+++ database.py
@@ -3,18 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-if not SQLALCHEMY_DATABASE_URL:
-# If the DATABASE_URL environment variable is not set, default to a local SQLite database.
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@postgresserver/db")
 
-engine_args = {}
-# The 'check_same_thread' argument is only needed for SQLite
-if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
-    engine_args["connect_args"] = {"check_same_thread": False}
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, **engine_args)
-Base = declarative_base()
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
